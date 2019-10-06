b9813ecf1fefec8810955d2e0e5ec437c274638e
einarzb <einarzb@gmail.com>
2019-10-06 15:16:08 +0300

    added babel for nice entries

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
-import { createStore, compose } from "../../node_modules/redux";
-import rootReducer from "./reducers/index";
+import { createStore, compose } from "redux";
+import rootReducer from "@reducers/index";
 
 export default createStore (
   rootReducer, 
