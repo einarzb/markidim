7b3f18a981142020a8a859664539a48d7230b549
einarzb <einarzb@gmail.com>
2019-11-10 15:04:41 +0200

    fixed mobile nav icons

--- src/components/UserButton.js
+++ src/components/UserButton.js
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 const UserButton = ({onClick}) => 
   <UserBtn type="button" data-toggle="collapse" data-target="#collapsingNavbar" onClick={onClick}>
      <img src={USER} height="20" width="20" style={{cursor:"pointer"}}/> 
-     <Text> שלום, עידן</Text>
+     <WelcomeText> שלום, עידן</WelcomeText>
   </UserBtn>
 
 export default UserButton;
@@ -21,15 +21,23 @@ direction:rtl;
 font-size:15px;
 lineHeight:1;
 height:50px;
- color:#FFFFFF;
-  align-items:center;
-  border:none;
-  cursor:pointer
+color:#FFFFFF;
+align-items:center;
+border:none;
+cursor:pointer
+  @media (max-width: 768px) {
+    position:absolute;
+    right: 14%;
+  }
 `;
 
-const Text = styled.div`
+const WelcomeText = styled.div`
 margin:0px 8px;
 background-color: transparent;
 font-size:15px;
 color:#FFFFFF;
+display:block;
+@media (max-width: 768px) {
+  display:none;
+}
 `;
